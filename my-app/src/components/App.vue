227cd12a8acdc9341061582ee2add34b66bdc0f5
Ak Md Ridhauddin <19FTT1151@student.pb.edu.bn>
2021-05-20 11:15:44 +0800

    edited tailwindcss

--- my-app/src/components/App.vue
+++ my-app/src/components/App.vue
@@ -4,10 +4,6 @@
     <ul>
       <li v-for="post in posts" :key="post.id">{{ post.content }}</li>
     </ul>
-    <div>
-      <input v-model="newPostContent" />
-      <button @click="addPost()">Add Post</button>
-    </div>
           <!-- This example requires Tailwind CSS v2.0+ -->
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -20,13 +16,7 @@
                       Name
                     </th>
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Title
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Status
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Role
+                      Student ID
                     </th>
                     <th scope="col" class="relative px-6 py-3">
                       <span class="sr-only">Edit</span>
@@ -42,10 +32,10 @@
                         </div>
                         <div class="ml-4">
                           <div class="text-sm font-medium text-gray-900">
-                            Jane Cooper
+                            {{ currentUser.username }}
                           </div>
                           <div class="text-sm text-gray-500">
-                            jane.cooper@example.com
+                            {{ post.content }}
                           </div>
                         </div>
                       </div>
@@ -53,14 +43,6 @@
                     <td class="px-6 py-4 whitespace-nowrap">
                       <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
                       <div class="text-sm text-gray-500">Optimization</div>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                      <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      Admin
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                       <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
@@ -75,6 +57,10 @@
         </div>
       </div>
 
+    <div>
+      <input v-model="newPostContent" />
+      <button @click="addPost()">Add Post</button>
+    </div>
   </div>
 </template>
 
