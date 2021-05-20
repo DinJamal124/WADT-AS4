c656fe6ac68848c5603f413b1bf66b485992c094
Ak Md Ridhauddin <19FTT1151@student.pb.edu.bn>
2021-05-20 08:58:16 +0800

    first other commit

---
+++ my-api/server.js
@@ -0,0 +1,104 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const schema = gql(`
+  type Query {
+    currentUser: User
+    postsByUser(userId: String!): [Post]
+  }
+
+  type Mutation {
+    addPost(content: String): Post 
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    content: String!
+    userId: ID!
+  }
+`);
+
+var data = {};
+
+data.posts = [
+  { 
+    id: 'xyz-1',
+    content: "First Post - Hello world",
+    userId: 'abc-1',
+  },
+  {
+    id: 'xyz-2',
+    content: "Second Post - Hello again",
+    userId: 'abc-2',
+  },
+  {
+    id: 'xyz-3',
+    content: "Third Post - Hello again",
+    userId: 'abc-3',
+  }
+];
+
+data.users = [
+  {
+    id: 'abc-1', 
+    username: "(AK MOHAMMAD RIDHAUDDIN BIN PG JAMALUDIN",
+  },
+  {
+    id: 'abc-2', 
+    username: "AMPUAN ZUHAIRAH BINTI AMPUAN HAJI ZAINAL",
+  },
+  {
+    id: 'abc-3', 
+    username: "MOHAMMAD HILMI BIN SABLI",
+  }
+];
+
+const currentUserId = 'abc-1';
+
+var resolvers = {
+  Mutation: {
+    addPost: async (_, { content }, { currentUserId, data }) => {
+      let post = { 
+        id: 'xyz-' + (data.posts.length + 1), 
+        content: content, 
+        userId: currentUserId,
+      };
+      data.posts.push(post);
+      return post;
+    }
+  },
+  Query: {
+    currentUser: (parent, args, context) => {
+      let user = context.data.users.find( u => u.id === context.currentUserId );
+      return user;
+    },
+    postsByUser: (parent, args, context) => {
+      let posts = context.data.posts.filter( p => p.userId === args.userId ); 
+      return posts
+    },
+  },
+  User: {
+    posts: (parent, args, context) => {
+      let posts = context.data.posts.filter( p => p.userId === parent.id );
+      return posts;
+    }
+  }
+};
+
+const server = new ApolloServer({ 
+  typeDefs: schema, 
+  resolvers: resolvers,
+  context: { 
+    currentUserId,
+    data
+  }
+});
+
+server.listen(4001).then(({ url }) => {
+  console.log('API server running at localhost:4001');
+});
